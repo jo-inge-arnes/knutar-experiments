fbf03bb55de1bbe1eb44c978bb1c6115a4a48b4c
Jo Inge Arnes <jo.i.arnes@uit.no>
2023-11-05 17:51:42 +0100

    figure for human penguin example

--- human-penguin-example.R
+++ human-penguin-example.R
@@ -2,35 +2,36 @@ library(knutar)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(ggpubr)
 library(ggh4x)
 library(scales)
 
 
 create_figure <- function(d, mod, title) {
   fig <- ggplot()
-  fig <- fig + theme_bw()
-  fig <- fig + geom_point(data = d, aes(age.dec, nwsize), shape = 1,
-    color = "gray50")
+  fig <- fig + theme_bw() +
+    theme(
+      panel.grid.major = element_blank(),  # Remove major grid lines
+      panel.grid.minor = element_blank()   # Remove minor grid lines
+    )
+  fig <- fig + geom_point(data = d, aes(age.dec, nwsize), shape = 1, color = "gray60", size = 0.6)
   fig <- fig + ggtitle(title)
-  fig <- fig + xlab(parse_format()("'Age Dec'"))
-  fig <- fig + ylab(parse_format()("'Nwsize'"))
+  fig <- fig + xlab("Age")
+  fig <- fig + ylab("Network size")
 
   knots <- extract_knots(mod)
 
-  fig <- fig + geom_vline(xintercept = knots$knots, linetype = "dashed")
-  fig <- fig + geom_vline(xintercept = knots$Boundary.knots, linetype = "solid")
+  fig <- fig + geom_vline(xintercept = knots$knots, linetype = "dashed", color = "gray40", size = 0.4)
+  fig <- fig + geom_vline(xintercept = knots$Boundary.knots, linetype = "solid", size = 0.4)
 
-  fig <- fig + geom_line(aes(x = mod$data$age.dec, y = mod$fitted.values),
-    linetype = "solid", color = "black", size = 0.5)
-
-  fig <- fig + force_panelsizes(rows = unit(3.5, "in"), cols = unit(7, "in"))
+  fig <- fig + geom_line(aes(x = mod$data$age.dec, y = mod$fitted.values), linetype = "solid", color = "gray20", size = 0.4)
 
   return(fig)
 }
 
 hpp.explore <- read.table("./explorepenguin_share_complete_cases.csv", sep=",", header=T)
 hpp.explore %>% 
-  mutate(age.years=2023-age, age.dec=age.years/10) -> d 
+  mutate(age.years=2023-age, age.dec=age.years) -> d 
 
 boundary_knots <-c(quantile(d$age.dec, .05), quantile(d$age.dec, .95))
 
@@ -40,9 +41,14 @@ m0 <- model_by_count(d, nwsize, age.dec, max_knots,
   boundary_knots = boundary_knots)
 m1 <- choose_splines(d, nwsize, age.dec,  max_nknots = max_knots, boundary_knots = boundary_knots)$model
 
-fig <- create_figure(d, m0, "quantiles")
-plot(fig)
+fig_standard <- create_figure(d, m0, "Standard")
+fig_ours <- create_figure(d, m1, "Ours")
 
-fig <- create_figure(d, m1, "ours")
-plot(fig)
+fig_standard_compared <- ggarrange(fig_standard, fig_ours, nrow = 2, ncol = 1)
+
+plot(fig_standard_compared)
+
+#ggsave("example_figs/human_penguin.png", plot = fig_standard_compared, device = "png", height = 4.2/2, width = 8.4)
+
+ggsave("example_figs/human_penguin.png", plot = fig_standard_compared, device = "png", height = 8/4*3, width = 8)
 
